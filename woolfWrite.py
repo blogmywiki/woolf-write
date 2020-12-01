bcb6317df24b45a3404e7cfc6f5bb70102ec7de6
blogmywiki <giles.booth@gmail.com>
2020-12-01 21:51:24 +0000

    various improvements
    Exception handling added for missing config file or if previously saved text file cannot be opened.

--- woolfWrite.py
+++ woolfWrite.py
@@ -1,7 +1,7 @@
 # a very simple text editor with word count made with guizero
-
 from guizero import App, TextBox, Text, PushButton
-app = App(title="WoolfWrite", width=1200, height=750)
+
+app = App(title="WoolfWrite", width=1400, height=800)
 
 def wordCount():
     status.value = "NOT SAVED"
@@ -23,17 +23,31 @@ def localSave():
     file.close()
     app.title = "WoolfWrite - " + filename
 
-file = open("woolfWrite.cfg","r")
-filename = file.read()
-file.close()
+# open config file with filename of last saved document
+try:
+    file = open("woolfWrite.cfg","r") 
+    filename = file.read()
+    file.close()
+except:
+    print("Could not open/read config file")
+    filename = "new document.txt"
 
-file = open(filename,"r")
-textContents = file.read()
-file.close()
+# if no saved document assign some text, otherwise open last document
+if filename == "new document.txt":
+    textContents = "It was a dark and stormy night..."
+else:
+    try:
+        file = open(filename,"r")
+        textContents = file.read()
+        file.close()
+    except:
+        print("Could not open/read text file " + filename)
+        textContents = "Error opening file " + filename
     
-input_box = TextBox(app, text=textContents, width=80, height=20, multiline=True, scrollbar=True, command=wordCount)
+input_box = TextBox(app, text=textContents, width=80, height=16, multiline=True, scrollbar=True, command=wordCount)
+input_box.font = "-apple-system" # MacOS system font, replace with something like Arial if not on a Mac
 input_box.text_color = "blue"
-input_box.text_size = 23
+input_box.text_size = 34
 input_box.bg = "#FFFFE7"
 wordText = Text(app, text="")
 saveButton = PushButton(app, command=localSave, text="Save local")
